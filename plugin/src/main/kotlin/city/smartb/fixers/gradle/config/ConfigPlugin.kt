af8fa6967629cfcc5125b2df3d276cf7f5bfec85
Adrien Poupard <adrien@smartb.city>
2022-12-02 15:28:37 +0100

    kt2Ts : Add cleaning regex

--- plugin/src/main/kotlin/city/smartb/fixers/gradle/config/ConfigPlugin.kt
+++ plugin/src/main/kotlin/city/smartb/fixers/gradle/config/ConfigPlugin.kt
@@ -5,10 +5,6 @@ import city.smartb.gradle.config.fixers
 import city.smartb.gradle.config.fixersIfExists
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.tasks.Copy
-import org.gradle.api.tasks.Delete
-import org.gradle.kotlin.dsl.invoke
-import org.gradle.kotlin.dsl.register
 
 class ConfigPlugin : Plugin<Project> {
 	override fun apply(target: Project) {
@@ -44,63 +40,6 @@ class ConfigPlugin : Plugin<Project> {
 		}
 	}
 
-	private fun Project.configureKt2Ts(mainConfig: ConfigExtension?) {
-		val target = this
-		mainConfig?.kt2Ts?.let { config ->
-			target.tasks {
-				register<Delete>("cleanTsGen") {
-					delete(config.outputDirectory)
-				}
-
-				register<Copy>("tsGen") {
-					target.logger.info("*** Start tsGen")
-					dependsOn("cleanTsGen")
-					val inputDir = if(config.inputDirectory != null) {
-						config.inputDirectory!!
-					} else {
-						"${this.project.buildDir.absolutePath}/js/packages/".also {
-							target.logger.info("fixers.kt2Ts.inputDirectory is not set. Default value [$it] will be used.")
-						}
-					}
-					from(inputDir) {
-						exclude("*-test")
-					}
-					into(config.outputDirectory)
-					includeEmptyDirs = false
-
-					val cleaning = listOf(
-						".d.ts" to listOf(
-							Regex("""(?m).*__doNotImplementIt.*\n""") to "",
-							Regex("""kotlin.js.""") to "",
-							Regex("""org.w3c.dom.url.""") to "",
-							Regex("""org.w3c.dom.""") to "",
-							Regex(""" any/\* ([^*/]*) \*/""") to " $1",
-							Regex("""type Nullable<T> = T \| null \| undefined\n""") to ""
-						),
-						"package.json" to listOf(
-							Regex("""("devDependencies": \{)(.|\n)*?(},)""") to "$1$3"
-						)
-					)
-
-					eachFile {
-						cleaning.forEach { (suffix, changes) ->
-							if (file.name.endsWith(suffix)) {
-								val content = file.readText()
-
-								val newContent = changes.fold(content) { acc, (old, new) ->
-									acc.replace(old, new)
-								}
-
-								if (newContent != content) {
-									file.writeText(newContent)
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-	}
 
 	private fun Project.createExtension() = extensions.findByType(ConfigExtension::class.java) ?: extensions.create(
 		ConfigExtension.NAME,
---
+++ plugin/src/main/kotlin/city/smartb/fixers/gradle/config/TsGenTask.kt
@@ -0,0 +1,68 @@
+package city.smartb.fixers.gradle.config
+
+import city.smartb.gradle.config.ConfigExtension
+import org.gradle.api.Project
+import org.gradle.api.tasks.Copy
+import org.gradle.api.tasks.Delete
+import org.gradle.kotlin.dsl.register
+import org.gradle.kotlin.dsl.invoke
+
+
+fun Project.configureKt2Ts(mainConfig: ConfigExtension?) {
+    val target = this
+    mainConfig?.kt2Ts?.let { config ->
+        target.tasks {
+            register<Delete>("cleanTsGen") {
+                delete(config.outputDirectory)
+            }
+
+            register<Copy>("tsGen") {
+                target.logger.info("*** Start tsGen")
+                dependsOn("cleanTsGen")
+                val inputDir = if(config.inputDirectory != null) {
+                    config.inputDirectory!!
+                } else {
+                    "${this.project.buildDir.absolutePath}/js/packages/".also {
+                        target.logger.info("fixers.kt2Ts.inputDirectory is not set. Default value [$it] will be used.")
+                    }
+                }
+                from(inputDir) {
+                    exclude("*-test")
+                }
+                into(config.outputDirectory)
+                includeEmptyDirs = false
+
+                val cleaning = listOf(
+                    ".d.ts" to listOf(
+                        Regex("""(?m).*__doNotImplementIt.*\n""") to "",
+                        Regex("""kotlin.js.""") to "",
+                        Regex("""org.w3c.dom.url.""") to "",
+                        Regex("""org.w3c.dom.""") to "",
+                        Regex(""" any/\* ([^*/]*) \*/""") to " $1",
+                        Regex("""type Nullable<T> = T \| null \| undefined\n""") to "",
+                        Regex("""(?<=\(|, |readonly )(\w*)(\?)?: Nullable<([\w\.<>, ]*)>(?=\)|, |;)""") to "$1?: $3"
+                    ),
+                    "package.json" to listOf(
+                        Regex("""("devDependencies": \{)(.|\n)*?(},)""") to "$1$3"
+                    )
+                )
+
+                eachFile {
+                    cleaning.forEach { (suffix, changes) ->
+                        if (file.name.endsWith(suffix)) {
+                            val content = file.readText()
+
+                            val newContent = changes.fold(content) { acc, (old, new) ->
+                                acc.replace(old, new)
+                            }
+
+                            if (newContent != content) {
+                                file.writeText(newContent)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
